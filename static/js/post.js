353d3187d9486f9cab138b2aa878fa32155539be
DDFeng <beanmrx@gmail.com>
2016-02-24 21:26:07 +0800

    add bshare supoort but not use it

--- static/js/post.js
+++ static/js/post.js
@@ -7,7 +7,7 @@
  */
 $(document).ready(function() {
     generateContent();
-    share();
+    {%if site.baidu_share %}share();{% endif %}
     duoshuo();
 });
 
@@ -22,12 +22,12 @@ function generateContent() {
         $toc.toc({ listType: 'ul', headers: 'h1, h2, h3' });
     });
 }
-
+{%if site.baidu_share %}
 function share(){
     window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"24"},"share":{}};
-    with(document)0[getElementsByTagName("script")[0].parentNode.appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
+    with(document)0[getElementsByTagName("script")[0].parentNode.appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v={{site.baidu_share}}.js?cdnversion='+~(-new Date()/36e5)];
 }
-
+{% endif %}
 var duoshuoQuery = {short_name:"{{ site.duoshuo }}"};
 function duoshuo(){
     var ds = document.createElement('script');
